dd40fec949ff4259ce3c902ce883cad2b89aeaea
rinsha <rinshajouhar5181@gmail.com>
2025-01-29 09:50:41 +0530

    post schema creation (#6)
    * post schema creation
    
    * post schema creation

---
+++ Model/PostSchema.ts
@@ -0,0 +1,77 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+interface IPost extends Document {
+  description?: string;
+  owner: Types.ObjectId;
+  images?: string;
+  lists: Types.ObjectId[];
+  likedBy: Types.ObjectId[];
+  reports: {
+    reportedBy: Types.ObjectId;
+    reason: string;
+    reportedAt: Date;
+  }[];
+  comments: {
+    user: Types.ObjectId;
+    comment: string;
+    commentedAt: Date; 
+    replies: {
+      user: Types.ObjectId;
+      reply: string;
+      repliedAt: Date;
+    }[];
+  }[];
+}
+
+const PostSchema = new Schema<IPost>(
+  {
+    description: { type: String, maxlength: 500 },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    likedBy: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    ],
+    reports: [
+      {
+        reportedBy: {
+          type: Schema.Types.ObjectId,
+          ref: "Users",
+          required: true,
+        },
+        reason: { type: String, required: true },
+        reportedAt: { type: Date, default: Date.now },
+      },
+    ],
+    comments: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "Users",
+          required: true,
+        },
+        comment: { type: String, required: true },
+        commentedAt: { type: Date, default: Date.now }, // Timestamp for comment
+        replies: [
+          {
+            user: {
+              type: Schema.Types.ObjectId,
+              ref: "Users",
+              required: true,
+            },
+            reply: { type: String, required: true },
+            repliedAt: { type: Date, default: Date.now }, // Timestamp for reply
+          },
+        ],
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model<IPost>("Post", PostSchema);

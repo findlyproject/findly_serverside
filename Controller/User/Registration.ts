4646eeb1a70d67fc80fccef9e07c10957756e5d3
sahathiya <salamkaraya4@gmail.com>
2025-01-29 11:49:16 +0530

    Registration page (#8)
    * creatin registration
    
    * userschema started
    
    * schema started
    
    * chnge registration schema
    
    * registration chaanged
    
    * schemacompleted

---
+++ Controller/User/Registration.ts
@@ -0,0 +1,58 @@
+import User from "../../Model/UserSchema";
+import { Request, Response } from "express";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+
+
+const RegistrationUser=async(req: Request, res: Response): Promise<void>=>{
+const{email,password,firstName,lastName,education,jobTitle,jobLocation}=req.body
+const emailRegex = /\S+@\S+\.\S+/;
+  if (!emailRegex.test(email)) {
+     res.status(400).json({ message: "Invalid email format" });
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+     res.status(400).json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+ const user= new User({email,password:hashedPassword,firstName,lastName,education,jobTitle,jobLocation})
+ await user.save();
+
+ const token = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.USER_SECRETKEY!,
+    { expiresIn: "1d" }
+  );
+  const refreshToken = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.USER_SECRETKEY!,
+    { expiresIn: "1d" }
+  );
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+ res.cookie("user", user, {
+    httpOnly: false,
+    secure: false,
+    sameSite: "lax",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  res.status(200).json({message:'success',user})
+}
+
+export{RegistrationUser}

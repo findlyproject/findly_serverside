866b10d5b5b45b049dfb88c58b6df8176a29a453
Aslah_c <aslah.c7070@gmail.com>
2025-01-31 12:04:49 +0530

    Curentuserapi (#12)
    * started to rating controller
    
    * want trycatch form moosa
    
    * trycatch middleware created

---
+++ Controller/commonFolders/Rating.ts
@@ -0,0 +1,27 @@
+import Rating from "../../Model/RatingSchema";
+import { Request, Response } from "express";
+
+
+ const createRating = async (req:Request, res:Response ):Promise<void> =>  {
+
+  const { review, starsRating, userId } = req.body;
+
+  if (!review || !starsRating || !userId) {
+     res.status(400).json({ message: "All fields are required." });
+  }
+
+  const newRating = new Rating({
+    review,
+    starsRating,
+    userId,
+  });
+
+  await newRating.save();
+  res.status(201).json({ message: "Rating created successfully", newRating });
+
+};
+
+
+
+
+export{createRating}

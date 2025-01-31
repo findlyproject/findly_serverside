d58f73abf01f07e12288f5738db5aa36122e3b1f
MOOSA SAHAD PK <165463409+moosasahad@users.noreply.github.com>
2025-01-31 15:23:04 +0530

    Loginapi (#22)
    * login complited
    
    * login complited wit trycatc midil ware
    
    ---------
    
    Co-authored-by: findlyproject <findlyproject@gmail.com>

--- Routes/user/registration.ts
+++ Routes/user/registration.ts
@@ -1,20 +1,13 @@
 import express from "express";
-<<<<<<< HEAD
 import { login, RegistrationUser } from "../../Controller/User/Registration";
-=======
-import { RegistrationUser } from "../../Controller/User/Registration";
 import { EmailUs } from "../../Controller/User/ContactUs";
->>>>>>> 479409c47c38a5679b84c05eee3d49c42adc6510
+import { errorCatch } from "../../Middleware/tryCatch";
 const router = express.Router()
 
 router
-.post("/registration",RegistrationUser)
-<<<<<<< HEAD
-.post("/login",login)
+.post("/registration", errorCatch(RegistrationUser))
+.post("/login",errorCatch(login))
 
-=======
-.post("/emailus",EmailUs)
->>>>>>> 479409c47c38a5679b84c05eee3d49c42adc6510
-
+.post("/emailus",errorCatch(EmailUs))
        
 export {router}

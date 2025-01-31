e7058944566721f6084323ee4f12cffcb45b1db8
findlyproject <findlyproject@gmail.com>
2025-01-31 15:20:38 +0530

    login complited wit trycatc midil ware

--- Routes/user/registration.ts
+++ Routes/user/registration.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { login, RegistrationUser } from "../../Controller/User/Registration";
 import { EmailUs } from "../../Controller/User/ContactUs";
+import { errorCatch } from "../../Middleware/tryCatch";
 const router = express.Router()
 
 router
-.post("/registration",RegistrationUser)
-.post("/login",login)
+.post("/registration", errorCatch(RegistrationUser))
+.post("/login",errorCatch(login))
 
-.post("/emailus",EmailUs)
-
+.post("/emailus",errorCatch(EmailUs))
        
 export {router}

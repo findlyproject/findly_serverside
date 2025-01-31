ffb88ed0a6b5a8b1cae04928929cd026c6d15be2
sahathiya <salamkaraya4@gmail.com>
2025-01-31 14:31:32 +0530

    Registration page (#16)
    chnaged contact us api

--- Routes/user/registration.ts
+++ Routes/user/registration.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { RegistrationUser } from "../../Controller/User/Registration";
+import { EmailUs } from "../../Controller/User/ContactUs";
 const router = express.Router()
 
 router
 .post("/registration",RegistrationUser)
+.post("/emailus",EmailUs)
 
        
 export {router}

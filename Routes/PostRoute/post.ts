7ba9f6e78669e27b332e80a2e90e2591ffd07bd5
Aslah7070 <aslah.c7070@gmail.com>
2025-02-01 12:02:07 +0530

    normalized post schema and added the type folder

--- Routes/PostRoute/post.ts
+++ Routes/PostRoute/post.ts
@@ -2,10 +2,13 @@ import express from "express";
 // import { addPost } from "../../Controller/commonFolders/postController/Post";
 import { upload } from "../../Middleware/upload";
 import {errorCatch} from '../../Middleware/tryCatch'
+import { replyToPost } from "../../Controller/commonFolders/postController/Replay";
+import { userAuthMiddleware } from "../../Middleware/userauthantication";
 const postRouter = express.Router();
 
 
 postRouter
 // .post("/post",upload.array("media", 5),errorCatch(addPost));
+.post("/user/postreplay/:postId",userAuthMiddleware,errorCatch(replyToPost))
 
 export default postRouter;

8f5718135c73df3bf278974b5e80bf00d00bdaa5
rinsha4145 <rinshajouhar5181@gmail.com>
2025-02-14 10:55:11 +0530

    standed the code

--- Controller/admin/Login.ts
+++ Controller/admin/Login.ts
@@ -1,54 +1,50 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 import { Admin } from "../../model/AdminSchema";
-import  jwt  from "jsonwebtoken";
-import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { CustomError } from "../../Utils/errorHandler";
 
 //login
-export const login = async(req:Request,res:Response):Promise<void>=>{
-    const { email, password } = req.body;
-    
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
 
-    if(!email || !password ){
-        res.status(404).json({status:false,message:"email and password is missing"})
-        return
-    }
-    const findAdmin = await Admin.findOne({email})
-    if(!findAdmin){
-        res.status(404).json({status:false,message:"Admin not found"})
-        return
-    }
-    const isMatch = await bcrypt.compare(password, findAdmin.password);
-    if (!isMatch) {
-      res.status(401).json({ status: false, message: "password not match" });
-      return;
-    }
-    const adminToken = jwt.sign(
-       
-            { id: findAdmin._id, email: findAdmin.email },
-            process.env.USER_SECRETKEY!,
-            { expiresIn: "2d" }
-          );
-          res.cookie("adminToken", adminToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "lax",
-            maxAge: 2*24 * 60 * 60 * 1000,
-          });
+  if (!email || !password) {
+    throw new CustomError("email and password is missing", 404);
+  }
+  const findAdmin = await Admin.findOne({ email });
+  if (!findAdmin) {
+    throw new CustomError("Admin not found", 404);
+  }
+  const isMatch = await bcrypt.compare(password, findAdmin.password);
+  if (!isMatch) {
+    throw new CustomError("password not match", 404);
+  }
+  const adminToken = jwt.sign(
+    { id: findAdmin._id, email: findAdmin.email },
+    process.env.USER_SECRETKEY!,
+    { expiresIn: "2d" }
+  );
+  res.cookie("adminToken", adminToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+    maxAge: 2 * 24 * 60 * 60 * 1000,
+  });
 
-          res.status(200).json({status:"success",message:"Admin login successful"})
-    
-}
+  res
+    .status(200)
+    .json({ status: "success", message: "Admin login successful" });
+};
+
 // logout
+export const logout = async (req: Request, res: Response): Promise<void> => {
+  res.clearCookie("adminToken", {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  });
 
-export const logout = async (req:Request,res:Response):Promise<void>=>{
-
-      res.clearCookie("adminToken",{
-        httpOnly:true,
-        secure:false,
-        sameSite:"lax",
-    }
-      )
-   
-    res.status(200).json({status:"success",message:"Admin logout sucssesfully"})
-
-}
+  res
+    .status(200)
+    .json({ status: "success", message: "Admin logout sucssesfully" });
+};

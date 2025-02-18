cc04861cdf5323af398730f19dd7b6bdc94cd9a0
Aslah7070 <aslah.c7070@gmail.com>
2025-02-18 14:09:57 +0530

    completed the api of deleteing editin post the jobpost

--- routes/compnayRoutes.ts
+++ routes/compnayRoutes.ts
@@ -5,7 +5,7 @@ import { upload } from "../middleware/upload";
 import { validateData } from "../middleware/zodValidation";
 import { CompanySchema, jobPostSchema, LoginSchema } from "../Utils/zodSchema";
 import { companyAuthMiddleware } from "../middleware/companyAuthentication";
-import { createJobPost, updateJobPost } from "../Controller/jobController/company";
+import { createJobPost, deleteJobPost, updateJobPost } from "../Controller/jobController/company";
 const companyRouter = express.Router();
 
 companyRouter
@@ -21,5 +21,6 @@ companyRouter
   .post("/logout",companyAuthMiddleware, errorCatch(logOut))
   .post("/jobposting",companyAuthMiddleware,validateData(jobPostSchema),errorCatch(createJobPost))
   .patch("/updatejobs/:jobId",companyAuthMiddleware,errorCatch(updateJobPost))
+  .delete("/deletejobpost/:jobId",companyAuthMiddleware,errorCatch(deleteJobPost))
 
 export { companyRouter };

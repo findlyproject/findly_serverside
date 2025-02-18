f625f8023119c28ae82a8aa9047c94289774654c
Aslah7070 <aslah.c7070@gmail.com>
2025-02-18 13:44:06 +0530

    job post api completed

--- routes/compnayRoutes.ts
+++ routes/compnayRoutes.ts
@@ -3,8 +3,9 @@ import { errorCatch } from "../middleware/tryCatch";
 import { login, logOut, register } from "../Controller/authController/company";
 import { upload } from "../middleware/upload";
 import { validateData } from "../middleware/zodValidation";
-import { CompanySchema, LoginSchema } from "../Utils/zodSchema";
+import { CompanySchema, jobPostSchema, LoginSchema } from "../Utils/zodSchema";
 import { companyAuthMiddleware } from "../middleware/companyAuthentication";
+import { createJobPost, updateJobPost } from "../Controller/jobController/company";
 const companyRouter = express.Router();
 
 companyRouter
@@ -18,5 +19,7 @@ companyRouter
   )
   .post("/login",validateData(LoginSchema),errorCatch(login))
   .post("/logout",companyAuthMiddleware, errorCatch(logOut))
+  .post("/jobposting",companyAuthMiddleware,validateData(jobPostSchema),errorCatch(createJobPost))
+  .patch("/updatejobs/:jobId",companyAuthMiddleware,errorCatch(updateJobPost))
 
 export { companyRouter };

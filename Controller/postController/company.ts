67f476f0443be8ca9fd8dec113803725b7cfb448
Aslah7070 <aslah.c7070@gmail.com>
2025-03-06 09:33:22 +0530

    completed handling jobposts

---
+++ Controller/postController/company.ts
@@ -0,0 +1,22 @@
+import  mongoose from "mongoose"
+import { Request, Response } from "express";
+import { Post } from "../../model/PostSchema";
+import { CustomError } from "../../Utils/errorHandler";
+
+export const getPostsByOwner = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
+    const ownerId  = req.user?.id
+    
+  
+    const posts = await Post.find({ owner: ownerId }).populate({path:"owner",match:{type:"Company"}});
+
+     
+    console.log("posts",posts);
+    
+    res
+      .status(200)
+      .json({ status: true, message: "Got the posts by the owner", posts });
+    return;
+  };
--- routes/compnayRoutes.ts
+++ routes/compnayRoutes.ts
@@ -19,6 +19,7 @@ import { createCompanyRating } from "../Controller/ratingController/user";
 
 
 import { deleteReview, deleteReviews, findreviewsBycompany, findreviewsByTargetedId } from "../Controller/ratingController/company";
+import { getPostsByOwner } from "../Controller/postController/company";
 
 const companyRouter = express.Router();
 
@@ -84,11 +85,18 @@ companyRouter
 
       .post(`/follow/:id`,userAuthMiddleware,errorCatch(FollowAndUnfollowCompany))
 
-
+      //GET POSTS
+      .get("/findposts",companyAuth,errorCatch(getPostsByOwner))
 
 
       //delete account
       .post("/accountdeletionreqst",companyAuth,errorCatch(requestDeleteAccount))
       .post("/verifyOtp",companyAuth,errorCatch(verifyOtp))
+       .get(
+          "/owner",
+          companyAuth,
+        
+          errorCatch(getPostsByOwner)
+        )
 
 export { companyRouter };
